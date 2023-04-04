97fa2f3791cdfee58fb17129435a691c6f54565a
Brenden Konnagan <brendenk1@icloud.com>
2023-04-03 23:05:29 -0400

    Fixed Package Naming

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "InkDryerKit",
+    name: "InkdryerKit",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "InkDryerKit",
-            targets: ["InkDryerKit"]),
+            name: "InkdryerKit",
+            targets: ["InkdryerKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "InkDryerKit",
+            name: "InkdryerKit",
             dependencies: []),
         .testTarget(
-            name: "InkDryerKitTests",
-            dependencies: ["InkDryerKit"]),
+            name: "InkdryerKitTests",
+            dependencies: ["InkdryerKit"]),
     ]
 )
